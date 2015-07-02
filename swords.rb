824927a84b6db4d6654d60ebf9401af2955d28ab
Lee Azzarello <lee@seriesdigital.com>
2015-07-02 06:24:04 -0400

    Near complete description for swords.

--- swords.rb
+++ swords.rb
@@ -9,21 +9,21 @@ class AceOfSwords
 
   def description
     desc = <<EOF
-Lorem ipsum
+A two-edged sword is held by a hand that emerges from the clouds.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+Conquest, triumph of power, great activity. The power to love strongly or to hate. Possible birth of a child who will be of heroic temperament.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Conquest, but the results are disastrous. Obstacles, great loss, infertility.
 EOF
     return puts mean
   end
@@ -40,21 +40,21 @@ class TwoOfSwords
 
   def description
     desc = <<EOF
-Lorem ipsum
+A seated woman balances two swords on her shoulders.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+Tension in relationships. Indecision. Balanced forces. Military friendships. A well-developed sense of balance and rhythm but needing direction. Stalemate.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Release, movement of affairs, sometimes in the wrong direction. Avoid impostors. Sympathy for these in trouble. Disloyalty.
 EOF
     return puts mean
   end
@@ -71,21 +71,22 @@ class ThreeOfSwords
 
   def description
     desc = <<EOF
-Lorem ipsum
+A heart pierced by three swords is seen against a stormy sky.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+Stormy weather for the affections. Tears, separation, quarreling. A general
+upheaval of the environment. Possibility of civil war. Political strife.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+There is still confusion, loss, sorrow, upheaval, but in lesser degree.
 EOF
     return puts mean
   end
@@ -102,21 +103,25 @@ class FourOfSwords
 
   def description
     desc = <<EOF
-Lorem ipsum
+Is a church with a stained-glass window, the effigy of a knight lies full
+length upon his tomb. His hands are clasped together in prayer.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+Hermit's repose. Rest after war. Banishment. Relaxation of anxiety, release
+from suffering. There will soon be a change for the better. Not a card of
+death.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Renewed activity; qualified success. Movement in one's affairs; social unrest.
+A caution to be wise, circumspect, and economical.
 EOF
     return puts mean
   end
@@ -133,21 +138,23 @@ class FiveOfSwords
 
   def description
     desc = <<EOF
-Lorem ipsum
+A knave has captured the swords of his adversaries, who depart in dejection.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+Failure, defeat. Degradation. Conquest of others. Unfairness, slander, cruelty,
+cowardliness.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Beware of pride. There is a chance of loss or defeat. Attendance at a funeral.
+Empty victory. Sorrow. Weakness.
 EOF
     return puts mean
   end
@@ -164,21 +171,24 @@ class SixOfSwords
 
   def description
     desc = <<EOF
-Lorem ipsum
+In a state of dejection, a woman and child are ferried across the water to a calm shore.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+The future will be better. Success after anxieties. Journey to a new home, by
+water. Sending someone to represent you in an undertaking. Also a journey in
+consciouness.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+You will stay where you are. No immediate way out of present difficulties.
+Unfavorable issue of a lawsuit or other undertaking.
 EOF
     return puts mean
   end
@@ -195,21 +205,22 @@ class SevenOfSwords
 
   def description
     desc = <<EOF
-Lorem ipsum
+A man is seen in the act of stealing away, carrying five swords from the military camp in the background. Two swords are still left impaled in the ground.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+A plan that may fail. An unwise attempt to make away with what is not yours.
+Unstable effort. Arguments over plans. Stying on another. Partial success.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Possibility of unexpected good. Sound advice. Instruction. Wishes about to be fulfilled.
 EOF
     return puts mean
   end
@@ -226,21 +237,23 @@ class EightOfSwords
 
   def description
     desc = <<EOF
-Lorem ipsum
+Again a maiden is "hoodwinked" (blindfolded), and this time she is also bound.
+She stands in a marshy place surrounded by swords. In the distance there is a
+castle high on a rocky cliff.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+Narrow or restricted surroundings. Bondage. Imprisonment through indecision. Betrayal. Fear to move out of a situation. Temporary sickness.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+New beginnings now possible. Impulsive, generous nature. Relaxation from fear. Freedom.
 EOF
     return puts mean
   end
@@ -257,21 +270,21 @@ class NineOfSwords
 
   def description
     desc = <<EOF
-Lorem ipsum
+Aeakening from sleep in the dead of night, a woman sits in despair, her head in her hands. Zodiacal signs decorate her coverlet. Nine swords hang heavy over her head.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+Suffering, loss, misery. Burden, oppression. Doubt and desolation. Illness. May mean death of a loved one.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Patience, unselfishness. Time brings healing. Tomorrow is another day.
 EOF
     return puts mean
   end
@@ -288,21 +301,27 @@ class TenOfSwords
 
   def description
     desc = <<EOF
-Lorem ipsum
+The swords in the other cards of this suit merely surrounded the person; here
+they have actually pierced him. He lies in a desolate waste, a black sky above
+him.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+Through the Nine of Swords showed despair, the meaning here is even more
+serious. Ruin of plans and projects. Defeat in war. Disruption of home life.
+Tears will fall. Trouble will come in spite of riches and high position. Not a
+card of violent death. A suggestion to give charity freely to those in trouble.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Overthrow of evil forces. Some success and profit. Courage to rise again. In
+spiritual matters, looking to higher powers for help.
 EOF
     return puts mean
   end
@@ -319,21 +338,29 @@ class PageOfSwords
 
   def description
     desc = <<EOF
-Lorem ipsum
+A youth (or maiden) holds a sword with both hands while walking swiftly over
+rough terrain. The wild clouds and the flight of a flock of birds give the
+feeling that trouble is brewing. The swords are the cards of warriors, of
+strife and misfortune. Choose this card for an energetic boy or girl with brown
+hair and brown eyes.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+A young person who might be a page in the diplomatic or government service. He
+or she already has great understanding and knows how to use diplomacy. This
+card can also mean a message and certain types of spying, as well as grace and
+dexterity.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Impostor, likely to be exposed. Frivolity and cunning. Possibility of ill
+health. Be prepared for the unexpected.
 EOF
     return puts mean
   end
@@ -350,21 +377,27 @@ class KnightOfSwords
 
   def description
     desc = <<EOF
-Lorem ipsum
+Here a knight dashes across open country, perhaps on a mission of romantic
+chivalry. Storm clouds are in the sky; cypress trees, sacred to Venus, are
+blown by the wind.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+A dashing, brave young man. Though he may be domineering, he has a clean heart
+and is full of courage. The card may also mean the coming or going of
+misfortune.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Given to braggadocio. Tyranny over the helpless, be it man or animal. A person
+always ready to start a fight. Destruction may come about through the knight's
+activities.
 EOF
     return puts mean
   end
@@ -381,21 +414,24 @@ class QueenOfSwords
 
   def description
     desc = <<EOF
-Lorem ipsum
+On a high throne, looking in to a clouded sky, sits a queen with a raised sword
+in her left hand.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+A subtle, keen, and quick-witted woman who may represent a widow or one who is
+unable to bear children. perhaps she is mourning for those she loves who are
+far away from her.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Unreliability. Narrow-mindedness. Gossip. Deceit. Malice. A woman of atrifice and prudery.
 EOF
     return puts mean
   end
@@ -412,21 +448,21 @@ class KingOfSwords
 
   def description
     desc = <<EOF
-Lorem ipsum
+A stern-looking kind is seated on the throne of judgment. Behind him is a pillar (or tapestry) with a butterfly design, signifying the soul.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+This is a man who may be a lawyer, a judge, a general, or a governor. He gives wise counsel, is firm in enmity as well as in friendship. A man of many ideas, thoughts and designs. The card may also mean power, strength, authority, or military intelligence. A lawsuit is in the offing.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Distrustful, suspicious. Also harsh and malicious. Plotting, barbarity, power for distuption, are revealed.
 EOF
     return puts mean
   end
