b027f8241c3e631774d142798f83b80e22fb3cb5
Lee Azzarello <lee@seriesdigital.com>
2015-07-01 23:22:46 -0400

    Add template for cups suit

---
+++ cups.rb
@@ -0,0 +1,465 @@
+class AceOfCups
+  attr_reader :name, :suit, :number
+
+  def initialize
+    @name = self.class
+    @suit = "Cups"
+    @number = 0
+  end
+
+  def description
+    desc = <<EOF
+Lorem ipsum
+EOF
+    return puts desc
+  end
+
+  def meaning
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+
+  def reversed
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+end
+
+class OneOfCups
+  attr_reader :name, :suit, :number
+
+  def initialize
+    @name = self.class
+    @suit = "Cups"
+    @number = 1
+  end
+
+  def description
+    desc = <<EOF
+Lorem ipsum
+EOF
+    return puts desc
+  end
+
+  def meaning
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+
+  def reversed
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+end
+
+class TwoOfCups
+  attr_reader :name, :suit, :number
+
+  def initialize
+    @name = self.class
+    @suit = "Cups"
+    @number = 2
+  end
+
+  def description
+    desc = <<EOF
+Lorem ipsum
+EOF
+    return puts desc
+  end
+
+  def meaning
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+
+  def reversed
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+end
+
+class ThreeOfCups
+  attr_reader :name, :suit, :number
+
+  def initialize
+    @name = self.class
+    @suit = "Cups"
+    @number = 3
+  end
+
+  def description
+    desc = <<EOF
+Lorem ipsum
+EOF
+    return puts desc
+  end
+
+  def meaning
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+
+  def reversed
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+end
+
+class FourOfCups
+  attr_reader :name, :suit, :number
+
+  def initialize
+    @name = self.class
+    @suit = "Cups"
+    @number = 4
+  end
+
+  def description
+    desc = <<EOF
+Lorem ipsum
+EOF
+    return puts desc
+  end
+
+  def meaning
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+
+  def reversed
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+end
+
+class FiveOfCups
+  attr_reader :name, :suit, :number
+
+  def initialize
+    @name = self.class
+    @suit = "Cups"
+    @number = 5
+  end
+
+  def description
+    desc = <<EOF
+Lorem ipsum
+EOF
+    return puts desc
+  end
+
+  def meaning
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+
+  def reversed
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+end
+
+class SixOfCups
+  attr_reader :name, :suit, :number
+
+  def initialize
+    @name = self.class
+    @suit = "Cups"
+    @number = 6
+  end
+
+  def description
+    desc = <<EOF
+Lorem ipsum
+EOF
+    return puts desc
+  end
+
+  def meaning
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+
+  def reversed
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+end
+
+class SevenOfCups
+  attr_reader :name, :suit, :number
+
+  def initialize
+    @name = self.class
+    @suit = "Cups"
+    @number = 7
+  end
+
+  def description
+    desc = <<EOF
+Lorem ipsum
+EOF
+    return puts desc
+  end
+
+  def meaning
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+
+  def reversed
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+end
+
+class EightOfCups
+  attr_reader :name, :suit, :number
+
+  def initialize
+    @name = self.class
+    @suit = "Cups"
+    @number = 8
+  end
+
+  def description
+    desc = <<EOF
+Lorem ipsum
+EOF
+    return puts desc
+  end
+
+  def meaning
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+
+  def reversed
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+end
+
+class NineOfCups
+  attr_reader :name, :suit, :number
+
+  def initialize
+    @name = self.class
+    @suit = "Cups"
+    @number = 9
+  end
+
+  def description
+    desc = <<EOF
+Lorem ipsum
+EOF
+    return puts desc
+  end
+
+  def meaning
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+
+  def reversed
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+end
+
+class TenOfCups
+  attr_reader :name, :suit, :number
+
+  def initialize
+    @name = self.class
+    @suit = "Cups"
+    @number = 10
+  end
+
+  def description
+    desc = <<EOF
+Lorem ipsum
+EOF
+    return puts desc
+  end
+
+  def meaning
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+
+  def reversed
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+end
+
+class PageOfCups
+  attr_reader :name, :suit, :number
+
+  def initialize
+    @name = self.class
+    @suit = "Cups"
+    @number = 11
+  end
+
+  def description
+    desc = <<EOF
+Lorem ipsum
+EOF
+    return puts desc
+  end
+
+  def meaning
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+
+  def reversed
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+end
+
+class KnightOfCups
+  attr_reader :name, :suit, :number
+
+  def initialize
+    @name = self.class
+    @suit = "Cups"
+    @number = 12
+  end
+
+  def description
+    desc = <<EOF
+Lorem ipsum
+EOF
+    return puts desc
+  end
+
+  def meaning
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+
+  def reversed
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+end
+
+class QueenOfCups
+  attr_reader :name, :suit, :number
+
+  def initialize
+    @name = self.class
+    @suit = "Cups"
+    @number = 13
+  end
+
+  def description
+    desc = <<EOF
+Lorem ipsum
+EOF
+    return puts desc
+  end
+
+  def meaning
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+
+  def reversed
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+end
+
+class KingOfCups
+  attr_reader :name, :suit, :number
+
+  def initialize
+    @name = self.class
+    @suit = "Cups"
+    @number = 14
+  end
+
+  def description
+    desc = <<EOF
+Lorem ipsum
+EOF
+    return puts desc
+  end
+
+  def meaning
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+
+  def reversed
+    mean = <<EOF
+Lorem ipsum
+EOF
+    return puts mean
+  end
+end
+
