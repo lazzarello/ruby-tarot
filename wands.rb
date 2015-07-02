c726c7c1dc870f8a5e2c2224ab1fd1ed192bd280
Lee Azzarello <lee@seriesdigital.com>
2015-07-02 05:47:46 -0400

    Get most of the wands information transcribed.

--- wands.rb
+++ wands.rb
@@ -9,21 +9,23 @@ class AceOfWands
 
   def description
     desc = <<EOF
-Lorem ipsum
+A hand comes out of a cloud, offering a flowering wand; eight detached leaves
+float in the air, suggesting Hebrew Yods or the decent of spirit into matter.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+Beginins of an enterprise, invention, or the beginning of a family. Perhaps the
+beginning of a journey, an adventure, an escapade.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Canceling of new enterprise, journey deferred, clouded joy, false starts.
 EOF
     return puts mean
   end
@@ -40,21 +42,26 @@ class TwoOfWands
 
   def description
     desc = <<EOF
-Lorem ipsum
+The Lord of Dominion looks from his battlements out over the sea. He holds a
+globe in his right hand and a staff in his left; another staff is held by an
+iron ring.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+Boldness, courage in embarking on an enterprise. The employment of scientific
+methods. Influence over another. Kindness and generosity -- yet a proud and
+unforgiving nature.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Restlessness, obstinacy, fear, physical suffering. The good beginning has
+turned against the Querent.
 EOF
     return puts mean
   end
@@ -71,21 +78,26 @@ class ThreeOfWands
 
   def description
     desc = <<EOF
-Lorem ipsum
+Here is an established merchant looking out to sea, as his ships come into
+port. He has accomplished what the man in the Two of Wands was just the
+beginning.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+Realization of hope, established strength, nobility, wealth, power. Caution
+against pride and arrogance. Partnership. Help will be offered by a successful
+merchant.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Beware of help offered. Wealth may slip away. There may be treachery and
+disappointment.
 EOF
     return puts mean
   end
@@ -102,21 +114,24 @@ class FourOfWands
 
   def description
     desc = <<EOF
-Lorem ipsum
+Bouquets of flowers are held high by two rejoicing maidens. A garland of
+celebration swings from the tops of four flowering wands. In the background is
+a castle with moat.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+Perfected work; rest after labor. Peace, prosperity, harmony. Romance, a coming marriage.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Though the meaning remains the same, there will not be quite the same fullness
+of peace, prosperity, and success.
 EOF
     return puts mean
   end
@@ -133,21 +148,25 @@ class FiveOfWands
 
   def description
     desc = <<EOF
-Lorem ipsum
+Five youths are fighting with huge wands. Are they in earnest? Since the fives
+in each of the four suits are negative, we can assume this is a serious
+quarrel. Remember, Wants mean enterprise, energy, but the five signifies that
+this energy is used negatively.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+Violent strife, rashness, competition. In the battle of life, boldness changes
+things for the better. Obstacles. Lawsuit.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Generosity. New business opportunities. Victory after surmounting obstacles.
 EOF
     return puts mean
   end
@@ -164,21 +183,23 @@ class SixOfWands
 
   def description
     desc = <<EOF
-Lorem ipsum
+This is the Lord of Victory, who comes riding by, carrying a laurel crown on
+his stabe. His men are meaning beside him.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+Good news. Victory after strife. Pleasure gained through labor. Success through
+industry. Advancement in the arts and sciences. Friends are helpful.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Rewards are delayed. Insolence of the victorious. Watch for successful enemy.
 EOF
     return puts mean
   end
@@ -195,21 +216,23 @@ class SevenOfWands
 
   def description
     desc = <<EOF
-Lorem ipsum
+The youth's stance is that of a yeoman fighting for his land. Six enemies are
+attacking from below.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+Victory through courage. Success against opposition. The Quereny has the
+position of advantage. Competition in business or trade. Certain success.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Ignorance, pretense. The Querent is threatened. Caution against indecision.
 EOF
     return puts mean
   end
@@ -226,21 +249,24 @@ class EightOfWands
 
   def description
     desc = <<EOF
-Lorem ipsum
+Having sped through open country, these eight wands are about to come to rest,
+their energy spent.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+Great haste, great hope, too rapid advancement. That which is on the move.
+Hasty communication. Messages, letters of love. Approach to a goal. Journey by
+air.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Delay, stagnation in business or affairs of the heart. Arrows of jealousy.
 EOF
     return puts mean
   end
@@ -257,21 +283,23 @@ class NineOfWands
 
   def description
     desc = <<EOF
-Lorem ipsum
+His bandaged head indicates the man has already fought and is prepared to fight
+again. The staves behind him sugest the boundary of that which he protects.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+The pause in a struggle. Preparedness, strength in reserve. Eventual victory
+but steady force must be applied. Obstinacy, strength, power, health.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Weakness, ill health, adversity, displeasure, obstacles to be overcome.
 EOF
     return puts mean
   end
@@ -288,21 +316,24 @@ class TenOfWands
 
   def description
     desc = <<EOF
-Lorem ipsum
+Here the young man is carrying ten staves, and the burden seems almost more
+than he can manage, yet he staggers on toward the city.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+Force and energy applied to selfish ends. Power unwisely used. Carrying a
+burden of ill-regulated power. Here is also a refining test by fire, the heart
+tried by pain. Problem soon to be solved.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Intrigues, separation, emigration. If a lawsuit is pending there will be some loss.
 EOF
     return puts mean
   end
@@ -319,21 +350,29 @@ class PageOfWands
 
   def description
     desc = <<EOF
-Lorem ipsum
+Some call the Pates princesses, and certainly from the picture this could be
+either a youth or a maiden who stands there boldly upholding a wand as if about
+to deliver a message. Three pyramids in the background are symbolic of the
+earth in its maternal aspect, and are also related to fire (as is the entire
+suit of Wands).
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+Courage, beauty. A nature sudden in love or anger. Desire for power;
+enthusiasm. A messenger or postman. A bearer of tidings. If this card appears
+in a spread next to one representing a man, there will be favorable testimony
+concerning him.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Cruel, unstable, domineering. If you are a woman, he may break your heart.
+Announcing bad news.
 EOF
     return puts mean
   end
@@ -350,21 +389,27 @@ class KnightOfWands
 
   def description
     desc = <<EOF
-Lorem ipsum
+A handsome young knight in armor gallops across the plains. There are three
+pyramids in the distance. His mantle is decorated with salamanders biting their
+tails. Salamanders supposedly can go through fire without being burned, and
+wands symbolize fire.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+A young man, possibly a warrior, filled with energy. He can be a generous
+friend or lover, but is also likely to be cruel and brutal. He is hasty in all
+he does. May also mean change of residence, emigration, quick departure.
+Knights in a spread may also mean the coming of going of a matter.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+The would-be lover turns jealous, creates conflict. Discord, frustration, lack of energy. Work interfered with.
 EOF
     return puts mean
   end
@@ -381,21 +426,26 @@ class QueenOfWands
 
   def description
     desc = <<EOF
-Lorem ipsum
+A crowned queen sits on her throne, holding a staff in her right hand.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+She has great power to attract that which she wants, and is fruitful in mind
+and body. Loving nature and her home, she is practical with money and sound in
+her business judgments. If she does not represent a woman, then she represents
+these qualities: success in undertakings; love of home and growing things;
+kindness and generosity.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Domineering, obstinate, revengeful; likely to turn suddenly against another
+without cause. If married, she could be unfaithful.
 EOF
     return puts mean
   end
@@ -412,21 +462,21 @@ class KingOfWands
 
   def description
     desc = <<EOF
-Lorem ipsum
+He sits in royal robes, a crown upon his head.
 EOF
     return puts desc
   end
 
   def meaning
     mean = <<EOF
-Lorem ipsum
+Handsome and passionate, he is agile in mind and body. A country gentleman, generally married. Loyal and noble, he comes of good family. Sometimes he is too hasty.
 EOF
     return puts mean
   end
 
   def reversed
     mean = <<EOF
-Lorem ipsum
+Intolerant, prejudiced, ill-natured. High-principled to a fault. Severe and ruthless.
 EOF
     return puts mean
   end
